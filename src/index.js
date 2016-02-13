ddd5acf2d42c6351a95caccc2315e1c3cfa5257c
Eli Morris-Heft <eli.morris.heft@gmail.com>
2016-02-13 01:12:26 -0600

    Create `npm run dev` and get React hot reloading going on it.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // Disable no-unused-vars because we're just importing these to get the CSS and Bootstrap JS.
 /* eslint-disable no-unused-vars */
 import BOOTSTRAP_CSS from 'bootstrap/dist/css/bootstrap.css';
-import MAIN_CSS from '../styles/main.css';
+import MAIN_CSS from './styles/main.css';
 
 import BOOTSTRAP_JS from 'bootstrap/dist/js/bootstrap.min';
 /* eslint-enable no-unused-vars */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from '../components/page/component.js';
+import Page from './components/page/component.js';
 
 ReactDOM.render(
   <Page />,
-  document.getElementById('content')
+  document.getElementById('root')
 );
